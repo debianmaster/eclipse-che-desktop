52d21cf6a57a1e99a593b53e65a01fc5afeb9b8e
tetradev1 <9chakri+ds@gmail.com>
2017-06-30 01:29:54 -0700

    with shelljs

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const electron = require('electron')
+var shell = require('shelljs')
+shell.config.execPath = shell.which('node')
+const child_process = require('child_process');
+var Docker = require('dockerode');
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -11,27 +17,80 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+function loadPage(page){
+   mainWindow = new BrowserWindow({width: 800, height: 600,frame:true})
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, page),
+      protocol: 'file:',
+      slashes: true
+    }))
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null
+      shell.exit(0);
+    })
+}
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  if (!shell.which('docker')) {
+    loadPage("pages/notinstalled.html");
+  }
+  else {
+    var container = docker.getContainer('che');
+    // query API for container info
+    container.inspect(function (err, data) {
+      if(err!=null){
+          docker.createContainer({
+            Image: 'eclipse/che',
+            Cmd: ['start'],
+            'Volumes': {
+              '/var/run/docker.sock': {},
+              '/data':{}
+            },
+            'Hostconfig': {
+              'Binds': ['/var/run/docker.sock:/var/run/docker.sock','/Users/jjonagam/che:/data']
+            }
+          }, function(err, container) {
+            container.attach({
+              stream: true,
+              stdout: true,
+              stderr: true,
+              tty: true
+            }, function(err, stream) {
+              stream.pipe(process.stdout);
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+              container.start(function(err, data) {
+                console.log("Done",err,data);
+              });
+            });
+          });
+      }
+    });
+    /*
+    if(shell.exec('docker inspect che',{async:false}).code !== 0) {
+      //loadPage("pages/runche.html");
+      /*child_process.spawnSync('docker', [ 'run', '--rm', '-ti', 'hello-world' ], {
+        stdio: 'inherit'
+      }); 
+      shell.exec("docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/che:/data eclipse/che start",{async:false,stdio: 'pipe'},function(code, stdout, stderr){
+          console.log('Exit code:', code);
+          console.log('Program output:', stdout);
+          console.log('Program stderr:', stderr);
+          if(code==0)
+          loadPage("index.html");
+      })
+      
+    }
+    else{
+      loadPage("index.html");
+    }
+    */
+  }
+  
 }
 
 // This method will be called when Electron has finished
@@ -58,3 +117,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const {ipcMain} = electron;
+
+ipcMain.on('close-main-window', function () {
+    app.quit();
+});
--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron')
+
+var closeEl = document.querySelector('.close');
+closeEl.addEventListener('click', function () {
+    ipcRenderer.send('close-main-window');
+});
